1412f735ffb11a9c0648d1f1ae081e622e8ac667
mimosahe <saohemmi@gmail.com>
2023-03-09 00:31:53 +0900

    0308 commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 
@@ -7,7 +7,7 @@ export const Weather = () => {
   const [description, setDescription] = useState(null);
   const [icon, setIcon] = useState(null);
 
-  const handleClick = () => {
+  useEffect(() => {
     // 現在地の緯度経度を取得（GoogleMap: Javascript API）
     function mapSuccess(position) {
       const lat = position.coords.latitude;
@@ -15,13 +15,12 @@ export const Weather = () => {
 
       // 緯度経度が取得できた場合は、市区町村名を取得（GoogleMap: Geocoding API）
       axios
-        .get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyATHkVtCki6nWBWZlQNK4fOd-uFp2dkJ2g")
+        .get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyDoIdJvQtpmpwGdTvGH-p_eUSEbVEsWTro")
         .then((response) => {
           let results = response.data.results[0].address_components.filter(function (component) {
             return component.types.indexOf("locality") > -1;
           }); // 市区町村は"locality"
           setCityName(results[0].long_name);
-          console.log(results[0]);
         })
         .catch((error) => {
           alert(error);
@@ -29,31 +28,30 @@ export const Weather = () => {
 
       // 緯度経度が取得できた場合は、本日の天気情報を取得（OpenWeather API）
       axios
-        .get("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=ac86a7c7f7e307421b5975639d34002d")
+        .get("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=42124b3bc9e2617b577ee2088659e8fd")
         .then((response) => {
           let description = response.data.weather[0].description;
           let iconImage = response.data.weather[0].icon + "@2x.png";
-          console.log("description", response.data);
-          // console.log("icon", iconImage);
+          // console.log("description", response.data);
           setIcon(iconImage);
           setDescription(description);
         })
         .catch((error) => {
           alert(error);
         });
-    }
+      }
 
     function mapFail(error) {
       alert("位置情報の取得に失敗しました。エラーコード：" + error.code);
     }
     navigator.geolocation.getCurrentPosition(mapSuccess, mapFail);
-  };
+  });
 
   return (
     <>
-      <button onClick={handleClick}>かくにんする</button>
+      {/* <button onClick={handleClick}>かくにんする</button> */}
       <p>{cityName}の天気</p>
-      <p>{description}</p>
+      {/* <p>{description}</p> */}
       {icon && (
         <div>
           <img src={`http://openweathermap.org/img/wn/${icon}`} />
