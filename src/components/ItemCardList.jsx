bf14e92eb408d6e81a621b3bf81d656ec0c4a16f
mimosahe <saohemmi@gmail.com>
2023-03-26 09:51:29 +0900

    0326 0951

--- src/components/ItemCardList.jsx
+++ src/components/ItemCardList.jsx
@@ -7,28 +7,32 @@ export const ItemCardList = () => {
   async function getAll() {
     const querySnapshot = await getDocs(collection(db, "items"));
     querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-      
+      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`)
+      const Items = doc.data();
+      console.log(Items)
+      // 試しにImgだけ
+      const ItemsImg = Items.map(function(item){return item.img});
     });
   }
+
   getAll();
-
-  {/* {items.map((item) => (
-        <div key={item.id}>{item.season}</div>
-      ))} */}
-  
-  const Items = [
-    {id:"", category1:"", category2:"", color:"", season:"", img:"https://firebasestorage.googleapis.com/v0/b/style-buddy-fb16a.appspot.com/o/image%2Fblouse_white.jpeg?alt=media&token=a996b4ac-500e-46d1-803e-8a6d0c4bbee0"},
-    {id:"", category1:"", category2:"", color:"", season:"", img:"https://firebasestorage.googleapis.com/v0/b/style-buddy-fb16a.appspot.com/o/image%2Fsweater_blue.jpeg?alt=media&token=13440aeb-4680-403d-b62a-4582d37894c6"},
-    {id:"", category1:"", category2:"", color:"", season:"", img:"https://source.unsplash.com/a-QH9MAAVNI"},
-    {id:"", category1:"", category2:"", color:"", season:"", img:"https://source.unsplash.com/164_6wVEHfI"},
-    {id:"", category1:"", category2:"", color:"", season:"", img:"https://source.unsplash.com/PqbL_mxmaUE"},
-  ];
-  const ItemsImg = Items.map(function(item){return item.img});
-
   return (
     <ul class="grid grid-cols-2 md:grid-cols-4 gap-5">
+      {/* 試しにImgだけ */}
       {ItemsImg.map((img) => <ItemCard img={img} />)}
     </ul>
   );
+  
+  // {items.map((item) => (
+  //     <div key={item.id}>{item.season}</div>
+  //   ))} 
+  
+  // const Items = [
+  //   {id:"", category1:"", category2:"", color:"", season:"", img:"https://firebasestorage.googleapis.com/v0/b/style-buddy-fb16a.appspot.com/o/image%2Fblouse_white.jpeg?alt=media&token=a996b4ac-500e-46d1-803e-8a6d0c4bbee0"},
+  //   {id:"", category1:"", category2:"", color:"", season:"", img:"https://firebasestorage.googleapis.com/v0/b/style-buddy-fb16a.appspot.com/o/image%2Fsweater_blue.jpeg?alt=media&token=13440aeb-4680-403d-b62a-4582d37894c6"},
+  //   {id:"", category1:"", category2:"", color:"", season:"", img:"https://source.unsplash.com/a-QH9MAAVNI"},
+  //   {id:"", category1:"", category2:"", color:"", season:"", img:"https://source.unsplash.com/164_6wVEHfI"},
+  //   {id:"", category1:"", category2:"", color:"", season:"", img:"https://source.unsplash.com/PqbL_mxmaUE"},
+  // ];
+  
 };
