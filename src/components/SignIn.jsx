142aa6dd80bbc2b97defd39517aa741201cb2f95
mimosahe <saohemmi@gmail.com>
2023-03-30 00:57:52 +0900

    0330 0057 commit

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,6 +4,7 @@ import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { auth } from '../firebase';
 import { Navigate } from "react-router-dom";
 import '../index.css';
+import { Footer } from "./Footer";
 
 export const SignIn = () => {
   const [signinEmail, setSigninEmail] = useState("");
@@ -41,7 +42,7 @@ export const SignIn = () => {
         <>
           <div class="bg-white py-6 sm:py-8 lg:py-12">
             <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-              <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Signin</h2>
+              <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Sign in</h2>
 
               <form onSubmit={handleSubmit} class="mx-auto max-w-lg rounded-lg border">
                 <div class="flex flex-col gap-4 p-4 md:p-8">
@@ -55,16 +56,17 @@ export const SignIn = () => {
                     <input name="password" type="password" value={signinPassword} onChange={(e) => setSigninPassword(e.target.value)} class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
                   </div>
 
-                  <button class="block rounded-lg bg-gray-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">Log in</button>
+                  <button class="block rounded-lg bg-gray-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">Sign in</button>
 
                 </div>
 
                 <div class="flex items-center justify-center bg-gray-100 p-4">
-                  <p class="text-center text-sm text-gray-500">Don't have an account? <a href="/signup" class="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Register</a></p>
+                  <p class="text-center text-sm text-gray-500">Don't have an account? <a href="/signup" class="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Sign up</a></p>
                 </div>
               </form>
             </div>
           </div>
+          {/* <Footer /> */}
         </>
       )}
     </>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -39,71 +39,34 @@ export const SignUp = () => {
         <Navigate to={`/`} />
       ) : (
         <>
-          <h1>新規登録</h1>
-          {/* ↓「onSubmit」を追加 */}
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>メールアドレス</label>
-              <input
-                name="email"
-                type="email"
-                value={registerEmail}
-                onChange={(e) => setRegisterEmail(e.target.value)}
-              />
+          <div class="bg-white py-6 sm:py-8 lg:py-12">
+            <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+              <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Sign up</h2>
+
+              <form onSubmit={handleSubmit} class="mx-auto max-w-lg rounded-lg border">
+                <div class="flex flex-col gap-4 p-4 md:p-8">
+                  <div>
+                    <label for="email" class="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
+                    <input name="email" type="email" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value)} class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                  </div>
+
+                  <div>
+                    <label for="password" class="mb-2 inline-block text-sm text-gray-800 sm:text-base">Password</label>
+                    <input name="password" type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
+                  </div>
+
+                  <button class="block rounded-lg bg-blue-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">Sign up</button>
+
+                </div>
+
+                <div class="flex items-center justify-center bg-gray-100 p-4">
+                  <p class="text-center text-sm text-gray-500">Already have an account? <a href="/signin" class="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Sign in</a></p>
+                </div>
+              </form>
             </div>
-            <div>
-              <label>パスワード</label>
-              <input
-                name="password"
-                type="password"
-                value={registerPassword}
-                onChange={(e) => setRegisterPassword(e.target.value)}
-              />
-            </div>
-            <button>登録する</button>
-          </form>
+          </div>
         </>
       )}
     </>
   );
 };
-
-
-// export const SignUp = () => {
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const { email, password } = event.target.elements;
-  //   // console.log(email.value, password.value);
-  //   createUserWithEmailAndPassword(auth, email.value, password.value)
-  //   .then((userCredential) => {
-  //     // Signed in
-  //     const user = userCredential.user;
-  //     // ...
-  //     alert("Singed up successfully");
-  //   })
-  //   .catch((error) => {
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     // ..
-  //   });
-  // };
-
-  // return (
-  //   <div>
-  //     <h1>ユーザ登録</h1>
-  //     <form onSubmit={handleSubmit}>
-  //       <div>
-  //         <label>メールアドレス</label>
-  //         <input name="email" type="email" placeholder="email" />
-  //       </div>
-  //       <div>
-  //         <label>パスワード</label>
-  //         <input name="password" type="password" placeholder="********"/>
-  //       </div>
-  //       <div>
-  //         <button>登録</button>
-  //       </div>
-  //     </form>
-  //   </div>
-  // );
-// };
