f1e3a6a64e521a6b33128d5d5f39df0deaa18d2d
mimosahe <saohemmi@gmail.com>
2023-04-01 11:25:29 +0900

    0401 1125 commit

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -33,7 +33,7 @@ export const Mypage = () => {
           {/* ↓ユーザーのメールアドレスを表示（ログインしている場合） */}
           <p>USER:  {user?.email}</p>
           <button onClick={signout} class="block rounded-lg bg-gray-800 px-2 py-1 text-center text-xs font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">
-          Sign Out</button>
+          Sign out</button>
 
           {/* メイン画面 */}
           <TodayPage />
