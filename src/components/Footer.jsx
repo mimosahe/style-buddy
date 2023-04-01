f12efa8276a6cb701e017f7184f1c14e37163665
mimosahe <saohemmi@gmail.com>
2023-04-01 15:27:18 +0900

    0401 1527 commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,13 @@ import '../index.css';
 
 export const Footer = () => {
     // const navigate = useNavigate();
-    const [active, setActive] = useState("addItems");
+    const [active, setActive] = useState("");
 
     return (
         <>
         <div class="absolute bottom-0 bg-white pt-5 sm:pt-5 lg:pt-24">
         {/* nav - start */}
         <nav class="sticky bottom-0 mx-auto flex w-full justify-between gap-8 border-t bg-white px-10 py-4 text-xs sm:max-w-md sm:rounded-t-xl sm:border-transparent sm:text-sm sm:shadow-2xl">
-            {/* <div class="flex flex-col items-center gap-1 text-indigo-500" onClick={()=> navigate('/')}> */}
-            {/* <div className="active" onClick={()=> navigate('/')}> */}
             <div className={`${active === "today" ? "active" : "inactive"}`} onClick={() => setActive('today')}>
             <FontAwesomeIcon icon={faSun} />
             {/* <span>Today</span> */}
@@ -22,17 +20,13 @@ export const Footer = () => {
             {/* </div> */}
             </div>
 
-            {/* <a href="additems" class="flex flex-col items-center gap-1 text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"> */}
             <div className={`${active === "addItems" ? "active" : "inactive"}`} onClick={() => setActive('addItems')}>
             <FontAwesomeIcon icon={faPlusCircle} />
-            {/* <span>Add Items</span> */}
             <a href="additems">Add Items</a>
             </div>
 
-            {/* <a href="itemlist?category=tops" class="flex flex-col items-center gap-1 text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"> */}
             <div className={`${active === "itemList" ? "active" : "inactive"}`} onClick={() => setActive('itemList')}>
             <FontAwesomeIcon icon={faTShirt} />
-            {/* <span>Item List</span> */}
             <a href="itemlist?category=tops">Item List</a>
             </div>
         </nav>
